f5616349f4ac053328560d34ea9473fe8603da8d
lkho20 <lankyho@outlook.com>
2020-02-24 14:10:27 -0500

    measure distance with sonar

--- Pseudo/checkSensors/checkSensors.ino
+++ Pseudo/checkSensors/checkSensors.ino
@@ -1,11 +1,21 @@
-void setup() {
-  // put your setup code here, to run once:
+#include "HCSR04.h"
 
+//uses HC-SR04 - https://github.com/Martinsos/arduino-lib-hc-sr04
+
+// Initialize sensor that uses digital pins 13 and 12.
+int triggerPin = 13;
+int echoPin = 12;
+UltraSonicDistanceSensor distanceSensor(triggerPin, echoPin);
+
+void setup () {
+  Serial.begin(9600);  // We initialize serial connection so that we could print values from sensor.
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop () {
+  // Every 500 miliseconds, do a measurement using the sensor and print the distance in centimeters.
+  double distance = distanceSensor.measureDistanceCm();
+  Serial.println(distance);
+  delay(500);
 }
 
 //Check Sensor
